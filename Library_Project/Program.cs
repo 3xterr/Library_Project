46b5db9a29dfad2176d9da49a6f1351546b96e2b
3xterr <177241359+3xterr@users.noreply.github.com>
2024-11-07 13:40:27 +0100

    Added a new function where user can now add a book to the library.

--- Library_Project/Program.cs
+++ Library_Project/Program.cs
@@ -8,45 +8,50 @@ namespace Library_Project
 
     class Book
     {
-        public int BookNumber { get; set; }
+
         public string Title { get; set; }
         public int Year { get; set; }
         public string Author { get; set; }
         public bool IsCheckedOut { get; set; }
+
+        
     }
 
     class Program
     {
-        //TODO: add method for adding and removing books through passing parametres into the methods.
+        //TODO: add method removing books.
         static void Main(string[] args)
         {
             List<Book> Library = new List<Book>();
-            Library.Add(new Book { BookNumber = 1, Title = "1984", Author = "George Orwell", Year = 1949, IsCheckedOut = false });
-            Library.Add(new Book { BookNumber = 2, Title = "To Kill a Mockingbird", Author = "Harper Lee", Year = 1960, IsCheckedOut = true });
-            Library.Add(new Book { BookNumber = 3, Title = "Pride and Prejudice", Author = "Jane Austen", Year = 1813, IsCheckedOut = false });
-            Library.Add(new Book { BookNumber = 4, Title = "Moby Dick", Author = "Herman Melville", Year = 1851, IsCheckedOut = false });
-            Library.Add(new Book { BookNumber = 5, Title = "The Great Gatsby", Author = "F. Scott Fitzgerald", Year = 1925, IsCheckedOut = false });
-            Library.Add(new Book { BookNumber = 6, Title = "Treasure Island", Author = "Robert Louis Stevenson", Year = 1883, IsCheckedOut = true });
-            Library.Add(new Book { BookNumber = 7, Title = "Fahrenheit 451", Author = "Ray Bradbury", Year = 1953, IsCheckedOut = false });
-            Library.Add(new Book { BookNumber = 8, Title = "The Wolf of Wall Street", Author = "Jordan Belfort", Year = 2007, IsCheckedOut = false });
-            Library.Add(new Book { BookNumber = 9, Title = "The Sound and the Fury", Author = "William Faulkner", Year = 1929, IsCheckedOut = true });
-            Library.Add(new Book { BookNumber = 10, Title = "The Catcher in the Rye", Author = "J.D. Salinger", Year = 1951, IsCheckedOut = true });
+            Library.Add(new Book { Title = "1984", Author = "George Orwell", Year = 1949, IsCheckedOut = false });
+            Library.Add(new Book { Title = "To Kill a Mockingbird", Author = "Harper Lee", Year = 1960, IsCheckedOut = true });
+            Library.Add(new Book { Title = "Pride and Prejudice", Author = "Jane Austen", Year = 1813, IsCheckedOut = false });
+            Library.Add(new Book { Title = "Moby Dick", Author = "Herman Melville", Year = 1851, IsCheckedOut = false });
+            Library.Add(new Book { Title = "The Great Gatsby", Author = "F. Scott Fitzgerald", Year = 1925, IsCheckedOut = false });
+            Library.Add(new Book { Title = "Treasure Island", Author = "Robert Louis Stevenson", Year = 1883, IsCheckedOut = true });
+            Library.Add(new Book { Title = "Fahrenheit 451", Author = "Ray Bradbury", Year = 1953, IsCheckedOut = false });
+            Library.Add(new Book { Title = "The Wolf of Wall Street", Author = "Jordan Belfort", Year = 2007, IsCheckedOut = false });
+            Library.Add(new Book { Title = "The Sound and the Fury", Author = "William Faulkner", Year = 1929, IsCheckedOut = true });
+            Library.Add(new Book { Title = "The Catcher in the Rye", Author = "J.D. Salinger", Year = 1951, IsCheckedOut = true });
 
+            
             void showMenu()
             {
                 bool running = true;
-                while (running = true)
+                while (running == true)
                 {
                     Console.WriteLine("=====================================");
                     Console.WriteLine("            Library Menu             ");
                     Console.WriteLine("=====================================");
                     Console.WriteLine("1. View all books");
                     Console.WriteLine("2. Borrow a book");
-                    Console.WriteLine("3. Exit");
+                    Console.WriteLine("3. Add a book to the library");
+                    Console.WriteLine("4. Remove a book from the library");
+                    Console.WriteLine("5. Exit");
                     Console.WriteLine("=====================================");
                     Console.Write("Please select an option: ");
 
-                    Console.Write("Enter a command: ");
+               
                     string command = Console.ReadLine();
                     switch (command)
                     {
@@ -54,12 +59,20 @@ namespace Library_Project
                             ViewBooks(); break;
                         
                         case "2":
-                            Console.Write("Enter the title of the book you want to borrow: ");
+                            Console.Write("Enter the nunber of the book you want to borrow: ");
                             int bookBorrow = Convert.ToInt32(Console.ReadLine());
                             BorrowByName(bookBorrow);
                             break;
-                        
+
                         case "3":
+                            AddBook();
+                            break;
+
+                        case "4":
+                            RemoveBook();
+                            break;
+
+                        case "5":
                             running = false; 
                             Console.WriteLine("Exiting the library system. Goodbye!");
                             Environment.Exit(0);
@@ -86,29 +99,51 @@ namespace Library_Project
             
             void BorrowByName(int bookBorrow)
             {
-               foreach (var title in Library)
+                int j = 1;
+                foreach (var book in Library)
                 {
-                    if (title.BookNumber == bookBorrow )
+                    if (bookBorrow == j)
                     {
-                        if (title.IsCheckedOut == true) 
+                        if (book.IsCheckedOut == true)
                         {
-                                     Console.WriteLine("We do not have this book in stock right now");
-                        } else { 
-                                             Console.WriteLine($"You have succesfully borrowed {title.Title}");
-                                             break;  
-                               }   
-                        
+                            Console.WriteLine("We do not have this book in stock.");
+                        } else
+                        {
+                            Console.WriteLine("You successfully borrowed the book.");
+                            book.IsCheckedOut = true;
+                        }
                     }
+                    j++;
+                }
+               
+            }
+            
+            void ViewBooks()
+            {
+                int i = 1;
+                foreach (var book in Library)
+                {
+                    Console.WriteLine($"{i} - {book.Title} by {book.Author} ({book.Year})");  
+                    i++;
                 }
             }
 
-            void ViewBooks()
+            void AddBook()
             {
-                foreach (var book in Library)
-                {
-                    Console.WriteLine($"{book.BookNumber} - {book.Title} by {book.Author} ({book.Year})");  
-                }
-            };
+                Console.WriteLine("\nEnter the name of the book.");
+                string newTitle = Console.ReadLine();
+                Console.WriteLine("\nEnter the name of the books' author.");
+                string newAuthor = Console.ReadLine();
+                Console.WriteLine("\nEnter the year in which book was published.");
+                int newYear = Convert.ToInt32(Console.ReadLine());
+                Library.Add(new Book { Title = newTitle, Author = newAuthor, Year = newYear, IsCheckedOut = false });
+                Console.WriteLine("\nNew book added!");
+            }
+
+            void RemoveBook()
+            {
+
+            }
         }
     }
 }
